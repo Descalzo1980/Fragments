86ef0574d83622f537e05919ea704129f7a44eab
Descalzo1980 <ragioniere1980@gmail.com>
2022-08-22 22:43:08 +0300

    first commit

---
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,firstFragment)
+            commit()
+        }
+
+        val myBtnFirst: Button = findViewById(R.id.btnFragment1)
+        myBtnFirst.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment,firstFragment)
+                commit()
+            }
+        }
+        val myBtnSecond: Button = findViewById(R.id.btnFragment2)
+        myBtnSecond.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment,secondFragment)
+                commit()
+            }
+        }
+    }
+}
